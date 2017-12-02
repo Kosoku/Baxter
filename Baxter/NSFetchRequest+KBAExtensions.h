272d880372056db6625642fc2099e419d70ec2fb
William Towe <willbur1984@gmail.com>
2017-12-01 23:38:46 -0500

    add KBA_fetchRequestWithOptions:

--- Baxter/NSFetchRequest+KBAExtensions.h
+++ Baxter/NSFetchRequest+KBAExtensions.h
@@ -17,10 +17,118 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/**
+ Typedef for fetch request options key that should be used with KBA_fetchRequestWithOptions:.
+ */
+typedef NSString* KBANSFetchRequestOptionsKey NS_STRING_ENUM;
+
+/**
+ The entity name. This should be an NSString.
+ 
+ @see [NSFetchRequest entityName]
+ */
+FOUNDATION_EXTERN KBANSFetchRequestOptionsKey const KBANSFetchRequestOptionsKeyEntityName;
+/**
+ Whether to include subentities. This should be a NSNumber wrapping a BOOL value.
+ 
+ @see [NSFetchRequest includesSubentities]
+ */
+FOUNDATION_EXTERN KBANSFetchRequestOptionsKey const KBANSFetchRequestOptionsKeyIncludesSubentities;
+/**
+ The NSPredicate to filter the fetch request.
+ 
+ @see [NSFetchRequest predicate]
+ */
+FOUNDATION_EXTERN KBANSFetchRequestOptionsKey const KBANSFetchRequestOptionsKeyPredicate;
+/**
+ The fetch limit. This should be a NSNumber wrapping an unsigned integer.
+ 
+ @see [NSFetchRequest fetchLimit]
+ */
+FOUNDATION_EXTERN KBANSFetchRequestOptionsKey const KBANSFetchRequestOptionsKeyFetchLimit;
+/**
+ The fetch offset. This should be a NSNumber wrapping an unsigned integer.
+ 
+ @see [NSFetchRequest fetchOffset]
+ */
+FOUNDATION_EXTERN KBANSFetchRequestOptionsKey const KBANSFetchRequestOptionsKeyFetchOffset;
+/**
+ The fetch batch size. This should be a NSNumber wrapping an unsigned integer.
+ 
+ @see [NSFetchRequest fetchBatchSize]
+ */
+FOUNDATION_EXTERN KBANSFetchRequestOptionsKey const KBANSFetchRequestOptionsKeyFetchBatchSize;
+/**
+ The NSArray of NSSortDescriptor instances for the fetch request.
+ 
+ @see [NSFetchRequest sortDescriptors]
+ */
+FOUNDATION_EXTERN KBANSFetchRequestOptionsKey const KBANSFetchRequestOptionsKeySortDescriptors;
+/**
+ The NSArray of NSString instances used for relationship prefetching.
+ 
+ @see [NSFetchRequest relationshipKeyPathsForPrefetching]
+ */
+FOUNDATION_EXTERN KBANSFetchRequestOptionsKey const KBANSFetchRequestOptionsKeyRelationshipKeyPathsForPrefetching;
+/**
+ The result type of the fetch request. This should be a NSNumber wrapping a NSFetchRequestResultType value.
+ 
+ @see [NSFetchRequest resultType]
+ */
+FOUNDATION_EXTERN KBANSFetchRequestOptionsKey const KBANSFetchRequestOptionsKeyResultType;
+/**
+ Whether to include pending changes. This should be a NSNumber wrapping a BOOL value.
+ 
+ @see [NSFetchRequest includesPendingChanges]
+ */
+FOUNDATION_EXTERN KBANSFetchRequestOptionsKey const KBANSFetchRequestOptionsKeyIncludesPendingChanges;
+/**
+ The NSArray of NSString instances for the properties to fetch.
+ 
+ @see [NSFetchRequest propertiesToFetch]
+ */
+FOUNDATION_EXTERN KBANSFetchRequestOptionsKey const KBANSFetchRequestOptionsKeyPropertiesToFetch;
+/**
+ Whether to return distinct results. This should be a NSNumber wrapping a BOOL value.
+ 
+ @see [NSFetchRequest returnsDistinctResults]
+ */
+FOUNDATION_EXTERN KBANSFetchRequestOptionsKey const KBANSFetchRequestOptionsKeyReturnsDistinctResults;
+/**
+ Whether to include property values. This should be a NSNumber wrapping a BOOL value.
+ 
+ @see [NSFetchRequest includesPropertyValues]
+ */
+FOUNDATION_EXTERN KBANSFetchRequestOptionsKey const KBANSFetchRequestOptionsKeyIncludesPropertyValues;
+/**
+ Whether to refresh refetched objects. This should be a NSNumber wrapping a BOOL value.
+ 
+ @see [NSFetchRequest shouldRefreshRefetchedObjects]
+ */
+FOUNDATION_EXTERN KBANSFetchRequestOptionsKey const KBANSFetchRequestOptionsKeyShouldRefreshRefetchedObjects;
+/**
+ Whether to return objects as faults. This should be a NSNumber wrapping a BOOL value.
+ 
+ @see [NSFetchRequest returnsObjectsAsFaults]
+ */
+FOUNDATION_EXTERN KBANSFetchRequestOptionsKey const KBANSFetchRequestOptionsKeyReturnsObjectsAsFaults;
+/**
+ The NSArray of NSString instances for the properties to group by.
+ 
+ @see [NSFetchRequest propertiesToGroupBy]
+ */
+FOUNDATION_EXTERN KBANSFetchRequestOptionsKey const KBANSFetchRequestOptionsKeyPropertiesToGroupBy;
+/**
+ The having NSPredicate.
+ 
+ @see [NSFetchRequest havingPredicate]
+ */
+FOUNDATION_EXTERN KBANSFetchRequestOptionsKey const KBANSFetchRequestOptionsKeyHavingPredicate;
+
 @interface NSFetchRequest (KBAExtensions)
 
 /**
- Create and return a NSFetchRequest with the provided parameters.
+ Create and return a NSFetchRequest with the provided parameters. Calls through to KBA_fetchRequestWithOptions: with the appropriate options dictionary.
  
  @param entityName The name of the entity to fetch
  @param predicate The predicate to apply to the fetch request
@@ -30,6 +138,13 @@ NS_ASSUME_NONNULL_BEGIN
  @return The fetch request
  */
 + (NSFetchRequest *)KBA_fetchRequestForEntityName:(NSString *)entityName predicate:(nullable NSPredicate *)predicate sortDescriptors:(nullable NSArray<NSSortDescriptor *> *)sortDescriptors limit:(NSUInteger)limit offset:(NSUInteger)offset;
+/**
+ Create and return a fetch request with the provided options.
+ 
+ @param options The dictionary of options to use when creating the fetch request
+ @return The fetch request
+ */
++ (NSFetchRequest *)KBA_fetchRequestWithOptions:(NSDictionary<KBANSFetchRequestOptionsKey, id> *)options;
 
 @end
 
--- Baxter/NSFetchRequest+KBAExtensions.m
+++ Baxter/NSFetchRequest+KBAExtensions.m
@@ -15,24 +15,96 @@
 
 #import "NSFetchRequest+KBAExtensions.h"
 
+KBANSFetchRequestOptionsKey const KBANSFetchRequestOptionsKeyEntityName = @"KBANSFetchRequestOptionsKeyEntityName";
+KBANSFetchRequestOptionsKey const KBANSFetchRequestOptionsKeyIncludesSubentities = @"KBANSFetchRequestOptionsKeyIncludesSubentities";
+KBANSFetchRequestOptionsKey const KBANSFetchRequestOptionsKeyPredicate = @"KBANSFetchRequestOptionsKeyPredicate";
+KBANSFetchRequestOptionsKey const KBANSFetchRequestOptionsKeyFetchLimit = @"KBANSFetchRequestOptionsKeyFetchLimit";
+KBANSFetchRequestOptionsKey const KBANSFetchRequestOptionsKeyFetchOffset = @"KBANSFetchRequestOptionsKeyFetchOffset";
+KBANSFetchRequestOptionsKey const KBANSFetchRequestOptionsKeyFetchBatchSize = @"KBANSFetchRequestOptionsKeyFetchBatchSize";
+KBANSFetchRequestOptionsKey const KBANSFetchRequestOptionsKeySortDescriptors = @"KBANSFetchRequestOptionsKeySortDescriptors";
+KBANSFetchRequestOptionsKey const KBANSFetchRequestOptionsKeyRelationshipKeyPathsForPrefetching = @"KBANSFetchRequestOptionsKeyRelationshipKeyPathsForPrefetching";
+KBANSFetchRequestOptionsKey const KBANSFetchRequestOptionsKeyResultType = @"KBANSFetchRequestOptionsKeyResultType";
+KBANSFetchRequestOptionsKey const KBANSFetchRequestOptionsKeyIncludesPendingChanges = @"KBANSFetchRequestOptionsKeyIncludesPendingChanges";
+KBANSFetchRequestOptionsKey const KBANSFetchRequestOptionsKeyPropertiesToFetch = @"KBANSFetchRequestOptionsKeyPropertiesToFetch";
+KBANSFetchRequestOptionsKey const KBANSFetchRequestOptionsKeyReturnsDistinctResults = @"KBANSFetchRequestOptionsKeyReturnsDistinctResults";
+KBANSFetchRequestOptionsKey const KBANSFetchRequestOptionsKeyIncludesPropertyValues = @"KBANSFetchRequestOptionsKeyIncludesPropertyValues";
+KBANSFetchRequestOptionsKey const KBANSFetchRequestOptionsKeyShouldRefreshRefetchedObjects = @"KBANSFetchRequestOptionsKeyShouldRefreshRefetchedObjects";
+KBANSFetchRequestOptionsKey const KBANSFetchRequestOptionsKeyReturnsObjectsAsFaults = @"KBANSFetchRequestOptionsKeyReturnsObjectsAsFaults";
+KBANSFetchRequestOptionsKey const KBANSFetchRequestOptionsKeyPropertiesToGroupBy = @"KBANSFetchRequestOptionsKeyPropertiesToGroupBy";
+KBANSFetchRequestOptionsKey const KBANSFetchRequestOptionsKeyHavingPredicate = @"KBANSFetchRequestOptionsKeyHavingPredicate";
+
 @implementation NSFetchRequest (KBAExtensions)
 
 + (NSFetchRequest *)KBA_fetchRequestForEntityName:(NSString *)entityName predicate:(NSPredicate *)predicate sortDescriptors:(NSArray *)sortDescriptors limit:(NSUInteger)limit offset:(NSUInteger)offset; {
-    NSParameterAssert(entityName);
+    NSMutableDictionary *options = [NSMutableDictionary dictionaryWithDictionary:@{KBANSFetchRequestOptionsKeyEntityName: entityName}];
     
-    NSFetchRequest *retval = [[NSFetchRequest alloc] initWithEntityName:entityName];
-    
-    if (predicate) {
-        [retval setPredicate:predicate];
+    if (predicate != nil) {
+        options[KBANSFetchRequestOptionsKeyPredicate] = predicate;
     }
-    if (sortDescriptors) {
-        [retval setSortDescriptors:sortDescriptors];
+    if (sortDescriptors != nil) {
+        options[KBANSFetchRequestOptionsKeySortDescriptors] = sortDescriptors;
     }
     if (limit > 0) {
-        [retval setFetchLimit:limit];
+        options[KBANSFetchRequestOptionsKeyFetchLimit] = @(limit);
     }
     if (offset > 0) {
-        [retval setFetchOffset:offset];
+        options[KBANSFetchRequestOptionsKeyFetchOffset] = @(offset);
+    }
+    
+    return [self KBA_fetchRequestWithOptions:options];
+}
++ (NSFetchRequest *)KBA_fetchRequestWithOptions:(NSDictionary<KBANSFetchRequestOptionsKey,id> *)options {
+    NSParameterAssert(options[KBANSFetchRequestOptionsKeyEntityName] != nil);
+    
+    NSFetchRequest *retval = [[NSFetchRequest alloc] initWithEntityName:options[KBANSFetchRequestOptionsKeyEntityName]];
+    
+    if (options[KBANSFetchRequestOptionsKeyIncludesSubentities] != nil) {
+        retval.includesSubentities = [options[KBANSFetchRequestOptionsKeyIncludesSubentities] boolValue];
+    }
+    if (options[KBANSFetchRequestOptionsKeyPredicate] != nil) {
+        retval.predicate = options[KBANSFetchRequestOptionsKeyPredicate];
+    }
+    if (options[KBANSFetchRequestOptionsKeyFetchLimit] != nil) {
+        retval.fetchLimit = [options[KBANSFetchRequestOptionsKeyFetchLimit] unsignedIntegerValue];
+    }
+    if (options[KBANSFetchRequestOptionsKeyFetchOffset] != nil) {
+        retval.fetchOffset = [options[KBANSFetchRequestOptionsKeyFetchOffset] unsignedIntegerValue];
+    }
+    if (options[KBANSFetchRequestOptionsKeyFetchBatchSize] != nil) {
+        retval.fetchBatchSize = [options[KBANSFetchRequestOptionsKeyFetchBatchSize] unsignedIntegerValue];
+    }
+    if (options[KBANSFetchRequestOptionsKeySortDescriptors] != nil) {
+        retval.sortDescriptors = options[KBANSFetchRequestOptionsKeySortDescriptors];
+    }
+    if (options[KBANSFetchRequestOptionsKeyRelationshipKeyPathsForPrefetching] != nil) {
+        retval.relationshipKeyPathsForPrefetching = options[KBANSFetchRequestOptionsKeyRelationshipKeyPathsForPrefetching];
+    }
+    if (options[KBANSFetchRequestOptionsKeyResultType] != nil) {
+        retval.resultType = [options[KBANSFetchRequestOptionsKeyResultType] unsignedIntegerValue];
+    }
+    if (options[KBANSFetchRequestOptionsKeyIncludesPendingChanges] != nil) {
+        retval.includesPendingChanges = [options[KBANSFetchRequestOptionsKeyIncludesPendingChanges] boolValue];
+    }
+    if (options[KBANSFetchRequestOptionsKeyPropertiesToFetch] != nil) {
+        retval.propertiesToFetch = options[KBANSFetchRequestOptionsKeyPropertiesToFetch];
+    }
+    if (options[KBANSFetchRequestOptionsKeyReturnsDistinctResults] != nil) {
+        retval.returnsDistinctResults = [options[KBANSFetchRequestOptionsKeyReturnsDistinctResults] boolValue];
+    }
+    if (options[KBANSFetchRequestOptionsKeyIncludesPropertyValues] != nil) {
+        retval.includesPropertyValues = [options[KBANSFetchRequestOptionsKeyIncludesPropertyValues] boolValue];
+    }
+    if (options[KBANSFetchRequestOptionsKeyShouldRefreshRefetchedObjects] != nil) {
+        retval.shouldRefreshRefetchedObjects = [options[KBANSFetchRequestOptionsKeyShouldRefreshRefetchedObjects] boolValue];
+    }
+    if (options[KBANSFetchRequestOptionsKeyReturnsObjectsAsFaults] != nil) {
+        retval.returnsObjectsAsFaults = [options[KBANSFetchRequestOptionsKeyReturnsObjectsAsFaults] boolValue];
+    }
+    if (options[KBANSFetchRequestOptionsKeyPropertiesToGroupBy] != nil) {
+        retval.propertiesToGroupBy = options[KBANSFetchRequestOptionsKeyPropertiesToGroupBy];
+    }
+    if (options[KBANSFetchRequestOptionsKeyHavingPredicate] != nil) {
+        retval.havingPredicate = options[KBANSFetchRequestOptionsKeyHavingPredicate];
     }
     
     return retval;
